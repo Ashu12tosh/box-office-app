d803159458225ddd06f7ca567f503d187f5402ab
Ashu12tosh <ash8707672263@gmail.com>
2023-07-05 23:20:23 +0530

    some syntax changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 const Home = () => {
-  const { inputValue, setInputValue } = useState('');
+  const [inputValue, setInputValue] = useState('');
   console.log(inputValue);
   const onInputChange = ev => {
     setInputValue(ev.target.value);
@@ -13,7 +13,9 @@ const Home = () => {
         onClick={() => {
           setInputValue('ashu');
         }}
-      ></button>
+      >
+        update to random
+      </button>
       <input type="text" value={inputValue} onChange={onInputChange} />
     </div>
   );
