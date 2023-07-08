9c197e1dd5e80ebd9ac04880c5730c7584673d7f
Ashu12tosh <ash8707672263@gmail.com>
2023-07-08 16:53:03 +0530

    added textbox for searching, search for shows button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,22 @@ const Home = () => {
   const onSearchInputChange = ev => {
     setsearchStr(ev.target.value);
   };
-const onSearch = (ev) => {
+  const onSearch = async (ev) => {
+    ev.preventDefault();
 
-}
+   const response = await fetch(`https://api.tvmaze.com/search/shows?q=$(searchStr)`);
+   const body = await response.json();
+console.log(body);
 
+    
+    // https://api.tvmaze.com/search/shows?q=boys
+  };
 
   return (
     <div>
-      < form onSubmit={onSearch} >
-      <input type="text" value={searchStr} onChange={onSearchInputChange} />
-      <button type="button">update to random</button>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
       </form>
     </div>
   );
